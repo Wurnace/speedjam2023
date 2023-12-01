9b0678bcbe56672f007c20be323502b5a07b2e98
Elijah Lawal <elijahlawal26@outlook.com>
2023-12-01 21:05:27 +0000

    wahoo

--- game/glider.gd
+++ game/glider.gd
@@ -1,5 +1,8 @@
-extends CSGBox3D
+extends StaticBody3D
 
+var rest_angle = Vector3.ZERO
+var speed = 10
+var velocity = Vector3.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +11,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("pivot_up"):
+		rotation.x += 1
+	if Input.is_action_pressed("pivot_down"):
+		rotation.x -= 1
+	
+	position += _calculate_velocity(speed) * delta
+	
+	
+
+func _calculate_velocity(speedin):
+	var angle = rotation.x
+	var outvel
+	outvel = Vector3(0, angle, speedin*angle)
+	return outvel
