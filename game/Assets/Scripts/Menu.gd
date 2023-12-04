a498d3e2294629bc51a708cbd3c6f9ccac714add
Imemyselfitb <kaidyn_rod@yahoo.com>
2023-12-04 07:51:38 +0000

    SPEEDJAM _ Menu

--- game/Assets/Scripts/Menu.gd
+++ game/Assets/Scripts/Menu.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 var background
+var Planets = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	background = $Background
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	background.global_rotation += 0.001
+
+func PlayGame():
+	get_tree().change_scene_to_packed(load("res://Scenes/Game.tscn"))
